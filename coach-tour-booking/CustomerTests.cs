07c216fdc9479886b38469e1f0b059882ee2c3c6
bsaranga <bsaranga@gmail.com>
2023-07-06 23:49:58 +0530

    planning 2

---
+++ coach-tour-booking/CustomerTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coach_tour_booking_tests
+{
+    [TestFixture]
+    public class CustomerTests
+    {
+
+    }
+}
