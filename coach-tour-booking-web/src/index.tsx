c26ce06e0941fb1b37c8dccb8a60438f45fa425c
bsaranga <bsaranga@gmail.com>
2023-07-10 22:59:18 +0530

    layout

--- coach-tour-booking-web/src/index.tsx
+++ coach-tour-booking-web/src/index.tsx
@@ -2,13 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "./index.css"
+import "./normalize.css";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  }
+])
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
-  <App />
+  <RouterProvider router={router}></RouterProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
