fabd43da32266f8171d87e64119de2d9a34dc78b
bsaranga <bsaranga@gmail.com>
2023-07-17 19:05:53 +0530

    added the email and html templates

---
+++ coach-tour-booking-api/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace coach_tour_booking_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("helloworld")]
+        public IActionResult SayHello()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
